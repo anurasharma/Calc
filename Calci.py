7b5169133c7eef1465e276eafeb2e69c1ab120e9
Sharma <anusharm@bmc.com>
2019-03-10 01:27:22 +0530

    First Commit code

---
+++ Calci.py
@@ -0,0 +1,12 @@
+num1=input("Enter the number: ")
+num2=input("Enter the another number: ")
+result= int(num1)+ int(num2)
+
+print(result)
+
+num3=input("Enter the Number: ")
+num4=input("Enter the another number: ")
+Result2= float(num3)+ float(num4)
+
+print(Result2)
+
